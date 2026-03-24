5a3795d525f73e8455b8adbeaec48d1f6bad4646
Medic0525 <Ryanandanny@gmail.com>
2026-03-24 15:27:59 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle = 'dark-content'/>
+      <View style = {styles.header}>
+        <Image source = "./assets/icon_menu.png" style = {styles.icon}/>
+        <Image source = "./assets/icon_search.png" style = {styles.icon}/>
+      </View>
+
+    </SafeAreaView>
   );
 }
+const GlobalStyles = {
+  spacing: {
+    vertical: 16,    
+    horizontal: 8,
+  },
+  colors: {
+    purple: '#6200EE', 
+    background: '#FFFFFF',
+    gray: '#666666',
+    black: '#131313',
+  },
+  fonts: {
+    family: "Roboto",
+    sizeh1: 24,
+    sizeh2: 16,
+    sizeb1: 14,
+    sizeb2: 12
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +40,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: GlobalStyles.spacing.vertical,
+    paddingHorizontal: GlobalStyles.spacing.horizontal,
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+    resizeMode: 'contain',
+  }
 });
