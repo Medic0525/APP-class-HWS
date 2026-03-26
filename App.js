351f50e37db79f9015855293f371764156efde77
Medic0525 <Ryanandanny@gmail.com>
2026-03-26 20:27:28 +0800

    emergency upload for restart computer

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView, TouchableOpacity, Platform } from 'react-native';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
 
+
+function HomeScreen({ navigation }) {
   const [activeTab, setActiveTab] = useState("home")
-
   return (
+
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle = 'dark-content'/>
       
@@ -27,22 +31,22 @@ export default function App() {
             {/*Book 1*/}
             <View style = {styles.bookcard}>
               <Image source = {require("./assets/img_book_fashinopolis.png")} style = {styles.bookpage}/>
-              <Text style = {styles.h2}>Fashionopolis</Text>
-              <Text style = {styles.p1}>Dana Thomas</Text>
+              <Text style = {[styles.h2, {marginTop: 16}]}>Fashionopolis</Text>
+              <Text style = {[styles.p1, {marginTop: 8}]}>Dana Thomas</Text>
             </View>
 
             {/*Book 2*/}
             <View style = {styles.bookcard}>
               <Image source = {require("./assets/img_book_chanel.png")} style = {styles.bookpage}/>
-              <Text style = {styles.h2}>Chanel</Text>
-              <Text style = {styles.p1}>Patrick Mauriès</Text>
+              <Text style = {[styles.h2, {marginTop: 16}]}>Chanel</Text>
+              <Text style = {[styles.p1, {marginTop: 8}]}>Patrick Mauriès</Text>
             </View>
 
             {/*Book 3*/}
             <View style = {styles.bookcard}>
               <Image source = {require("./assets/img_book_calligraphy.png")} style = {styles.bookpage}/>
-              <Text style = {styles.h2}>Calligraphy</Text>
-              <Text style = {styles.p1}>June & Lucy</Text>
+              <Text style = {[styles.h2, {marginTop: 16}]}>Calligraphy</Text>
+              <Text style = {[styles.p1, {marginTop: 8}]}>June & Lucy</Text>
             </View>
 
           </ScrollView>
@@ -55,14 +59,14 @@ export default function App() {
           <ScrollView horizontal showsHorizontalScrollIndicator = {false} contentContainerStyle = {styles.scrollGroup}>
 
             {/*Book 1*/}
-            <View style = {styles.bookcard}>
+            <TouchableOpacity style = {styles.bookcard} onPress = {() => navigation.navigate("Details")}>
               <Image source = {require("./assets/img_book_ysl.png")} style = {styles.bookpage}/>
               <View style = {styles.rating}>
                 {[1,2,3,4,5].map(x => (<Image key = {x} source = {x <= 4 ? require("./assets/icon_star_filled.png") : require("./assets/icon_star_empty.png")}/>))}
               </View>
-              <Text style = {styles.h2}>Yves Saint Laurent</Text>
-              <Text style = {styles.p1}>Dana Thomas</Text>
-            </View>
+              <Text style = {[styles.h2, {marginTop: 8}]}>Yves Saint Laurent</Text>
+              <Text style = {[styles.p1, {marginTop: 8}]}>Dana Thomas</Text>
+            </TouchableOpacity>
 
             {/*Book 2*/}
             <View style = {styles.bookcard}>
@@ -70,8 +74,8 @@ export default function App() {
               <View style = {styles.rating}>
                 {[1,2,3,4,5].map(x => (<Image key = {x} source = {x <= 3 ? require("./assets/icon_star_filled.png") : require("./assets/icon_star_empty.png")}/>))}
               </View>
-              <Text style = {styles.h2}>The Book of Signs</Text>
-              <Text style = {styles.p1}>Patrick Mauriès</Text>
+              <Text style = {[styles.h2, {marginTop: 8}]}>The Book of Signs</Text>
+              <Text style = {[styles.p1, {marginTop: 8}]}>Patrick Mauriès</Text>
             </View>
 
             {/*Book 3*/}
@@ -80,8 +84,8 @@ export default function App() {
               <View style = {styles.rating}>
                 {[1,2,3,4,5].map(x => (<Image key = {x} source = {x <= 3 ? require("./assets/icon_star_filled.png") : require("./assets/icon_star_empty.png")}/>))}
               </View>
-              <Text style = {styles.h2}>Stitched Up</Text>
-              <Text style = {styles.p1}>June & Lucy</Text>
+              <Text style = {[styles.h2, {marginTop: 8}]}>Stitched Up</Text>
+              <Text style = {[styles.p1, {marginTop: 8}]}>June & Lucy</Text>
             </View>
 
           </ScrollView>
@@ -93,24 +97,114 @@ export default function App() {
       <View style = {styles.nav}>
 
         {/*home*/}
-        <TouchableOpacity onPress={() => setActiveTab("home")}>
-          <Image source = {activeTab === "home" ? require("./assets/icon_home.png") : require("./assets/icon_home_actived.png")}/>
+        <TouchableOpacity style = {styles.navItem} onPress={() => setActiveTab("home")}>
+          <Image style = {styles.navIcon} source = {activeTab === "home" ? require("./assets/icon_home_actived.png") : require("./assets/icon_home.png")}/>
+          <Text style={[styles.navText, activeTab === "home" && styles.navTextActive]}>Home</Text>
         </TouchableOpacity>
       
         {/*wishlist*/}
-        <TouchableOpacity onPress={() => setActiveTab("wishlist")}>
-          <Image source = {activeTab === "home" ? require("./assets/icon_nav_bookmark.png") : require("./assets/icon_nav_bookmark_actived.png")}/>
+        <TouchableOpacity style = {styles.navItem} onPress={() => setActiveTab("wishlist")}>
+          <Image style = {styles.navIcon} source = {activeTab === "wishlist" ? require("./assets/icon_nav_bookmark_actived.png") : require("./assets/icon_nav_bookmark.png")}/>
+          <Text style={[styles.navText, activeTab === "wishlist" && styles.navTextActive]}>Wishlist</Text>
         </TouchableOpacity>
 
         {/*mybook*/}
-        <TouchableOpacity onPress={() => setActiveTab("mybook")}>
-          <Image source = {activeTab === "home" ? require("./assets/icon_mybook.png") : require("./assets/icon_mybook_actived.png")}/>
+        <TouchableOpacity style = {styles.navItem} onPress={() => setActiveTab("mybook")}>
+          <Image style = {styles.navIcon} source = {activeTab === "mybook" ? require("./assets/icon_mybook_actived.png") : require("./assets/icon_mybook.png")}/>
+          <Text style={[styles.navText, activeTab === "mybook" && styles.navTextActive]}>My books</Text>
         </TouchableOpacity>
       </View>
 
     </SafeAreaView>
   );
 }
+
+function DetailsScreen({navigation}){
+  const [activeTab, setActiveTab] = useState("home")
+  const [bookMarked, setBookMarked] = useState(false)
+  return(
+    <SafeAreaView style = {styles.infoContainer}>
+      <StatusBar barStyle = 'dark-content'/>
+      
+      {/*header*/}
+      <View style = {styles.header}>
+        <TouchableOpacity onPress = {() => navigation.goBack()}>
+          <Image source = {require("./assets/icon_back.png")} style = {styles.icon}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress = {() => setBookMarked(!bookMarked)}>
+          <Image source = {bookMarked ? require("./assets/icon_bookmark_actived.png") : require("./assets/icon_bookmark.png")} style = {styles.icon}/>
+        </TouchableOpacity>
+      </View>
+
+      {/*main area*/}
+      <View>
+        
+        <View style = {styles.bigPic}>
+          <Image source = {require("./assets/img_book_ysl.png")} style = {{ width: 210, height: 300 }}/>
+        </View>
+
+        <View style = {styles.bookInfo}>
+          <Text style = {[styles.h1, ]}>Yves Saint Laurent</Text>
+          <Text style = {[styles.p1, {color: GlobalStyles.colors.gray}]}>Suzy Menkes </Text>
+          <View style = {styles.infoRating}>
+            <View style = {styles.rating}>
+                {[1,2,3,4,5].map(x => (<Image key = {x} source = {x <= 4 ? require("./assets/icon_star_filled.png") : require("./assets/icon_star_empty.png")}/>))}
+            </View>
+            <Text style = {styles.p2}>4.0 / 5.0</Text>
+          </View>
+        </View>
+
+        <View style = {styles.infoDesc}>
+          <Text style = {styles.p1}>A spectacular visual journey through 40 years of haute couture from one of the best-known and most trend-setting brands in fashion.</Text>
+        </View>
+
+      </View>
+
+      <TouchableOpacity style={styles.buyButton}>
+          <Text style={styles.buyButtonText}>BUY NOW FOR $46.99</Text>
+      </TouchableOpacity>
+
+      {/*nav bar*/}
+      <View style = {styles.nav}>
+
+        {/*home*/}
+        <TouchableOpacity style = {styles.navItem} onPress={() => setActiveTab("home")}>
+          <Image style = {styles.navIcon} source = {activeTab === "home" ? require("./assets/icon_home_actived.png") : require("./assets/icon_home.png")}/>
+          <Text style={[styles.navText, activeTab === "home" && styles.navTextActive]}>Home</Text>
+        </TouchableOpacity>
+      
+        {/*wishlist*/}
+        <TouchableOpacity style = {styles.navItem} onPress={() => setActiveTab("wishlist")}>
+          <Image style = {styles.navIcon} source = {activeTab === "wishlist" ? require("./assets/icon_nav_bookmark_actived.png") : require("./assets/icon_nav_bookmark.png")}/>
+          <Text style={[styles.navText, activeTab === "wishlist" && styles.navTextActive]}>Wishlist</Text>
+        </TouchableOpacity>
+
+        {/*mybook*/}
+        <TouchableOpacity style = {styles.navItem} onPress={() => setActiveTab("mybook")}>
+          <Image style = {styles.navIcon} source = {activeTab === "mybook" ? require("./assets/icon_mybook_actived.png") : require("./assets/icon_mybook.png")}/>
+          <Text style={[styles.navText, activeTab === "mybook" && styles.navTextActive]}>My books</Text>
+        </TouchableOpacity>
+      </View>
+
+    </SafeAreaView>
+  )
+}
+
+
+
+
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const GlobalStyles = {
   spacing: {
     vertical: 16,    
@@ -169,31 +263,51 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    paddingVertical: GlobalStyles.spacing.vertical,
-    paddingHorizontal: GlobalStyles.spacing.horizontal,
-    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+    marginTop: 32,
     height: 56
   },
 
-  icon: {
-    width: 24,
-    height: 24,
-    paddingVertical: 8,
-    paddingHorizontal: 8,
-    resizeMode: 'contain'
+  icon:{
+    margin: 8
   },
 
   sectionTitle: {
     paddingVertical: GlobalStyles.spacing.vertical,
-    paddingHorizontal: GlobalStyles.spacing.horizontal,
     marginLeft: 20,
     marginTop: 8
   },
 
   scrollGroup: {
     paddingHorizontal: 20
+  },
+
+  bookcard: {
+    marginRight: 16
+  },
+
+
+
+  rating: {
+    flexDirection: "row",
+    marginTop: 16
+  },
+
+  nav: {
+    flexDirection: "row",
+    height: 56,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginBottom: 42,
+  },
+  
+  navItem: {
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+
+  navTextActive:{
+    color: GlobalStyles.colors.purple
   }
-
-
-
 });
